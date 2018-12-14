63ae491a924ec78a508d4a848979de3b7d299c64
smhongok <smhongok@gmail.com>
2018-12-15 02:53:01 +0900

    6pm

--- include/project2/pid.cpp
+++ include/project2/pid.cpp
@@ -12,9 +12,9 @@ PID::PID(){
      *
     */
 
-	Kp=3.0;
-	Ki=0.00;
-	Kd=2.5;
+	Kp=4.0;
+	Ki=0;//0.006;
+	Kd=0;//30;
 //	Ki=0;	Kd=0;
 	error = 0.0;
 	error_sum = 0.0;
@@ -53,20 +53,22 @@ float PID::get_control(point car_pose, point goal_pose){
 		if(error<= -M_PI) error += 2*M_PI;
 	else if(error>=M_PI) error -= 2*M_PI;
 
+	error_sum += pre_error;
+	error_diff =  error-pre_error;
 
 	if(sqrt(pow(car_pose.x-goal_pose.x,2)+pow(car_pose.y-goal_pose.y,2))<0.2) {
-        //	reset();
+        	reset();
 	}
 
+	if(pre_error*error < 0) {reset();}
 
-
-	error_sum += pre_error;
-	error_diff =  pre_error - error;
-//	printf("P I D %.2f %.2f %.2f \n",Kp*error, Ki*error_sum, Kd*error_diff);
+	printf("P I D cartogoal head %.2f %.2f %.2f %.2f %.2f\n ",Kp*error,Ki*error_sum,  Kd*error_diff,  angle_from_car_to_goal, heading_angle);
 
 	ctrl = Kp* error + Ki*error_sum + Kd*error_diff;  
-	if(ctrl>=4.0) ctrl=4.0;
-	else if(ctrl<=-4.0) ctrl=-4.0;
+	if(ctrl>=2.0) ctrl=2*ctrl-2;
+	else if(ctrl<=-2.0) ctrl=2+2*ctrl;
+	if(ctrl>=3.0) ctrl=4.0;
+	else if(ctrl<=-3.0) ctrl=-4.0;
 
 	pre_x=car_pose.x;
 	pre_y=car_pose.y;
